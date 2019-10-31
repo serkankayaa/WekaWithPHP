64cb955da89e274f96de5f2177c084cc0abcf965
Serkan Kaya <srknnkaya@gmail.com>
2019-10-31 10:12:29 +0300

    removed whitespaces

--- WekaWithPhp/AAP.php
+++ WekaWithPhp/AAP.php
@@ -1,9 +1,9 @@
 <?php
 $resultArray=array();
+
 function AAP($str) {
   $result="";
   $homepage = file_get_contents('FArray.txt');
-
   $FArray = explode("\n",$homepage);
 
   for($i = 0 ; $i < count($FArray); $i++){
@@ -21,35 +21,30 @@ function AAP($str) {
     }
   }
 
-
   for($i = 0; $i < 400; $i++) {
-    for($j = 0; $j < strlen($str) - 1; $j++) {
-
+      for($j = 0; $j < strlen($str) - 1; $j++) {
         $tempDC = substr($str, $j, 2);
 
-      if(strcmp($tempDC, $resultArray["inf"][$i]) == 0) {
-        $counter++;
+        if(strcmp($tempDC, $resultArray["inf"][$i]) == 0) {
+          $counter++;
+        }
+      }
+        $resultArray["val"][$i] = $counter;
+        $counter = 0;
+    }
+
+    for($x = 0; $x < 400; $x++) {
+      $resultArray["val"][$x] = number_format($FArray[$x] * $resultArray["val"][$x],5);
+      if($resultArray["val"][$x] == -0 || is_nan($resultArray["val"][$x])) {
+        $resultArray["val"][$x] = 0;
       }
     }
-      $resultArray["val"][$i] = $counter;
-      $counter = 0;
 
-  }
-
-  for($x = 0; $x < 400; $x++) {
-    $resultArray["val"][$x] = number_format($FArray[$x] * $resultArray["val"][$x],5);
-    if($resultArray["val"][$x] == -0 || is_nan($resultArray["val"][$x])) {
-      $resultArray["val"][$x] = 0;
+    for ($i=0; $i <count($resultArray["val"]) ; $i++) { 
+      $resultArray["val"][$i]=number_format($resultArray["val"][$i],5);
+      $result .= $resultArray["val"][$i] . ',';
     }
+
+    return $result;
   }
-for ($i=0; $i <count($resultArray["val"]) ; $i++) { 
-  $resultArray["val"][$i]=number_format($resultArray["val"][$i],5);
-  $result .= $resultArray["val"][$i] . ',';
-}
-  return $result;
-
-
-}
-
-
 ?>
